3c79dc54e71615caaf1ce1a861be6795d3236078
Vitaly Kovalenko <rtm_wrk@rambler.ru>
2023-06-13 01:06:37 +0300

    Test fail -> set error value in date filed

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -24,9 +24,9 @@ class RegistrationTest {
     void shouldRegisterDebetCard() {
         // Подготовим данные для работы с элементами формы заказа дебетовой карты
         // Формируем значения поля "Город"
-        String cityRegistration = "Краснодар!";
+        String cityRegistration = "Краснодар";
         // Формируем значение поля "Дата" - по условию дата должна быть не раньше чем текущая + 3 дня
-        String dateVisit = setupDate(4, "dd.MM.yyyy");
+        String dateVisit = setupDate(1, "dd.MM.yyyy");
         // Формируем значение поля "Фамилия и Имя клиента"
         String nameUser = "Иванов Петр";
         // Формируем значение поля "Телефон клиента"
